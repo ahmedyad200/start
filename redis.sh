d0b171bf2e7a49707c2fdfb0332885db9873194f
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-05-04 08:58:38 +0200

    Add files via upload

---
+++ redis.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# contact me @ahmedyad200
+
+echo -e "\n\nThis Script is written by: AHMED @ahmedyad200\n\n"
+
+sleep 4
+
+apt-get update
+
+apt-get upgrade
+
+echo -e "\n\ninstalling redis-server ......\n\n"
+
+apt install redis-server
+
+echo -e "\n\nTurning off redis-server ......\n\n"
+
+service redis stop
+
+read -p "Enter the old server-ip-address -> " ip
+
+echo -e "\n\nRunning scp command with -> $ip ip address\n\n"
+
+sudo scp root@$ip:/var/lib/redis/dump.rdb /var/lib/redis/dump.rdb
+
+echo -e "\n\nRestarting redis-server ......\n\n"
+
+service redis start
+
+echo -e "\n\nDone Installing New Redis DB\nScript By: @ahmedyad200"
+
+echo -e "\nAfter 5 Sec will run redis-cli keys test\n\n"
+
+sleep 5
+
+redis-cli keys "*"
