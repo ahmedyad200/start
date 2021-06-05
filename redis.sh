aac45327775fe0cf34f684cca2f2097d57778338
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-06-06 01:43:16 +0200

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -28,10 +28,12 @@ echo -e "\n\nRestarting redis-server ......\n\n"
 
 service redis start
 
-echo -e "\n\nDone Installing New Redis DB\nScript By: @ahmedyad200"
+echo -e "\n\nDone Installing New Redis DB"
 
 echo -e "\nAfter 5 Sec will run redis-cli keys test\n\n"
 
 sleep 5
 
 redis-cli keys "*"
+
+io.write("\27[31;47m\n◼¦ Script By: AHMEDYAD *@ahmedyad200 ¦◼\27[0;34;49m\n")  
