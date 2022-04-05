0ca3e88f4588f8050dbaf288dce868b29755767b
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2022-04-06 01:26:23 +0200

    Update Host.sh

--- Host.sh
+++ Host.sh
@@ -1,6 +1,6 @@
 echo -e "\n\nThis Script is written by: AHMED @ahmedyad200\n\n"
 cd 
-sudo apt install apache2
+sudo apt install apache2 -y
 sudo systemctl enable apache2
 sudo systemctl start apache2
 sudo systemctl stop apache2
@@ -8,34 +8,32 @@ echo -e "\n\nInstalld apache2\n\n"
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
-sudo apt-get install php7.4 php7.4-mysql php7.4-zip php7.4-curl php7.4-mbstring php7.4-json php7.4-cli php7.4-dev
+sudo apt-get install php7.4 php7.4-mysql php7.4-zip php7.4-curl php7.4-mbstring php7.4-json php7.4-cli php7.4-dev -y
 sudo chmod -R 777 /var/www/html
-sudo apt install php-cli unzip
-sudo apt install php-mbstring
-apt install composer
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo apt install php-cli unzip -y
+sudo apt install php-mbstring -y
+sudo apt install composer -y
+# sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 echo -e "\n\nsetup php host\n\n"
-composer
+# composer
 cd ~
 mkdir slugify
 cd slugify
 composer require cocur/slugify
 cd ..
-apt search mbstring
+sudo apt search mbstring
 composer update
 composer update vendor/package vendor2/package2
 sudo apt install libjson-c-dev
 cd ..
 sudo apt-get install -y php-json
 sudo apt-get update -y
-sudo apt-get install emboss bioperl ncbi-blast+ gzip libjson-perl libtext-csv-perl libfile-slurp-perl liblwp-protocol-https-perl libwww-perl
-apt install npm
+sudo apt-get install emboss bioperl ncbi-blast+ gzip libjson-perl libtext-csv-perl libfile-slurp-perl liblwp-protocol-https-perl libwww-perl -y
+sudo apt install npm -y
 npm i json
 npm install -g json
-cd ~/bin
+cd /bin
 curl -L https://github.com/trentm/json/raw/master/lib/json.js > json ; chmod 755 json
-apt update
-apt upgrade
 cd /etc/php/7.4/apache2
 rm -fr php.ini
 wget https://raw.githubusercontent.com/ahmedyad200/YYYBD/master/php.ini
